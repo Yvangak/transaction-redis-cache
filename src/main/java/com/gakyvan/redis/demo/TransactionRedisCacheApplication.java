a50d4955f5b7c7499b2f247b884f009cb75e90cb
Yvan GAKUBA <gakyvan@gmail.com>
2020-06-22 21:30:03 -0400

    initial commit

---
+++ src/main/java/com/gakyvan/redis/demo/TransactionRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.gakyvan.redis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionRedisCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gakyvan/redis/demo/TransactionRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gakyvan.redis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
